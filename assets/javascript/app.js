83d52125a835da51965ac6371d5dd1eafbceaa91
Steve Archibald <stevearchi21@gmail.com>
2019-06-10 22:11:14 -0600

    added more styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
 //vars for correct answers
 var ans = [document.getElementById('cor1'), document.getElementById('cor2'), 
 document.getElementById('cor3'), document.getElementById('cor4'), document.getElementById('cor5')];
-var score = 0;
+var totalCorrect = 0;
+var totalIncorrect = 0;
+var timedOut = false;
 function timeRemaining(timeRemaining) {
     time = 30;
     setInterval(function () {
@@ -11,39 +13,41 @@ function timeRemaining(timeRemaining) {
 }
 
 function check() {
-    var results = [i];
-    console.log(document.getElementById('cor1').checked)
-    console.log(ans[0].checked)
-    score = 0;
+    totalCorrect = 0;
+    totalIncorrect = 0;
     for (var i = 0; i < ans.length; i ++){
         if (ans[i].checked){
-            results[i] = 'You got question ' + parseInt(i+1) + ' correct!';
-            score++;
+            totalCorrect++;
         }else{
-            results[i] = 'You got question ' + parseInt(i+1) + ' wrong';
+            totalIncorrect++;
         } 
     };
-
-    return results;
 }
 // timer for game
 var timeGame = setTimeout(function () {
     // display which answers are incorrect and correct
-
+    timedOut = true;
+    checkandDisplay(); 
 }, 30000);
 
+function displayResults(){
+    var finalScore = Math.floor(100 * parseInt(totalCorrect)/parseInt(ans.length));
+    $('#main').append('<p> You scored a ' + finalScore + '%');
+    $('#main').append('<p>You answered ' + totalCorrect + ' questions correctly</p>');
+    $('#main').append('<p>You answered ' + totalIncorrect + ' questions incorrectly</p>');
+}
+function checkandDisplay(){
+    // clear display
+    $('#main').empty();
+    if (timedOut){
+        $('#main').append("<h2>Time is up!</h2>")
+    }
+    check();
+    displayResults();
+}
 $('#submit').on('click', function (event) {
-
     event.preventDefault();
-    var result = check();
-    var finalScore = Math.floor(100 * parseInt(score)/parseInt(ans.length));
-    $('#results').html('<p> You scored a ' + finalScore + '%');
-   
-    
-    // loops through results and prints them  
-   for (i=0; i<result.length; i++){
-    $('#results').append('<p>' + result[i] + '</p>');
-   }
+    checkandDisplay(); 
 });
 
 $("#timeRemain").text('Time Remaining: 30');
